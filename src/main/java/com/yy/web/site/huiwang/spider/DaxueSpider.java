c7736120ca17f022e399af883a27d27f220c8b0f
ttluowen <luowen@yiyuen.com>
2018-09-01 16:31:36 +0800

    采集大学

---
+++ src/main/java/com/yy/web/site/huiwang/spider/DaxueSpider.java
@@ -0,0 +1,102 @@
+package com.yy.web.site.huiwang.spider;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.GzipDecompressingEntity;
+import org.apache.http.client.methods.HttpGet;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.yy.log.Logger;
+import com.yy.util.file.FileUtil;
+import com.yy.util.map.MapValue;
+import com.yy.util.streamreader.BufferedInReader;
+import com.yy.util.string.StringUtil;
+import com.yy.web.config.SystemConfig;
+import com.yy.web.site.spider.Spider;
+
+public class DaxueSpider {
+
+	private static void mainInit() {
+
+		String root = System.getProperty("user.dir") + "\\target\\huiwang-1.0\\";
+		SystemConfig.setSystemPath(root);
+		SystemConfig.setWebInfPath(root + "WEB-INF\\");
+
+
+		Logger.setSystemPath(root);
+	}
+
+	public static void main(String[] args) throws UnsupportedOperationException, IOException {
+		
+		mainInit();
+		
+		
+		List<MapValue> school = new ArrayList<>();
+
+		int pageSize = 30;
+		int totalPage = 9999;
+		int recordCount = 0;
+		HttpClient client = Spider.getClient();
+
+		
+		for (int page = 1; page <= totalPage; page++) {
+			String a = "https://data-gkcx.eol.cn/soudaxue/queryschool.html?messtype=json&callback=jQuery18309684761254968921_1535765528517&province=&schooltype=&page=" + page + "&size=" + pageSize + "&keyWord1=&schoolprop=&schoolflag=&schoolsort=&schoolid=&_=1535765545206";
+			HttpGet get = new HttpGet(a);
+			get.addHeader("Referer", "https://gkcx.eol.cn/soudaxue/queryschool.html?&province=&page=" + page);
+	
+			HttpResponse response = client.execute(get);
+			HttpEntity entity = response.getEntity();
+	
+			for (Header header : response.getAllHeaders()) {
+				if (header.getName().equals("Content-Encoding")) {
+					if (header.getValue().toLowerCase().equals("gzip")) {
+						entity = new GzipDecompressingEntity(entity);
+					}
+				}
+			}
+
+			String b = BufferedInReader.read(entity.getContent(), StringUtil.UTF8);
+			JSONObject obj = JSON.parseObject(b);
+			JSONArray schoolList = obj.getJSONArray("school");
+
+			for (Object item : schoolList) {
+				JSONObject it = (JSONObject) item;
+				String name = it.getString("schoolname");
+				String province = it.getString("province");
+				String description = it.getString("jianjie");
+				String historyNames = it.getString("oldname");
+				
+				MapValue map = new MapValue();
+				map.put("name", name);
+				map.put("province", province);
+				map.put("description", description);
+				map.put("historyNames", historyNames);
+				
+				school.add(map);
+			}
+
+
+			if (recordCount == 0) {
+				recordCount = obj.getJSONObject("totalRecord").getIntValue("num");
+				totalPage = (int) Math.ceil((double) recordCount / (double) pageSize);
+			}
+		}
+
+
+		StringBuffer fileContent = new StringBuffer();
+		String sql = "INSERT INTO school(name, historyNames, type, province, description) VALUES('{name}', '{historyNames}', '大学', '{province}', '{description}');";
+		for (MapValue item : school) {
+			fileContent.append(StringUtil.substitute(sql, item)).append("\n");
+		}
+		
+		FileUtil.save(SystemConfig.getSystemPath() + "out/daxue.sql", fileContent.toString());
+	}
+}
---
+++ src/main/java/com/yy/web/site/spider/Spider.java
@@ -0,0 +1,140 @@
+package com.yy.web.site.spider;
+
+import java.io.IOException;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.GzipDecompressingEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+
+import com.yy.log.Logger;
+import com.yy.util.streamreader.BufferedInReader;
+import com.yy.util.string.StringUtil;
+import com.yy.util.url.UrlUtil;
+
+/**
+ * 蜘蛛爬虫。
+ * 
+ * @since 2018-04-23
+ * @version 1.0
+ * @author Luowen
+ */
+@SuppressWarnings("deprecation")
+public class Spider {
+	
+	private static HttpClient client;
+	
+	
+	/**
+	 * 获取 HTTP 客户端对象。
+	 * 
+	 * @return
+	 */
+	public static HttpClient getClient() {
+
+		if (client == null) {
+			client = new DefaultHttpClient(new ThreadSafeClientConnManager());			
+		}
+		
+		
+		return client;
+	}
+
+	
+	/**
+	 * 获取指定页面的文本内容。
+	 * 
+	 * @param url
+	 * @param charset
+	 * @param catchSleep
+	 * @return
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	public static String getUrlContent(String url, String charset, int catchSleep) {
+		
+		return getUrlContent(url, charset, catchSleep, true);
+	}
+
+
+	/**
+	 * 获取指定页面的文本内容。
+	 * 
+	 * @param url
+	 * @param charset
+	 * @param catchSleep
+	 * @param tryAgain
+	 * 
+	 * @return
+	 * @throws ClientProtocolException
+	 * @throws IOException
+	 */
+	public static String getUrlContent(String url, String charset, int catchSleep, boolean tryAgain) {
+
+		String content = "";
+
+		try {
+			if (catchSleep > 0) {
+				Thread.sleep(catchSleep);
+			}
+
+
+			url = url.replaceAll(" ", "%20");
+
+			HttpGet get = new HttpGet(url);
+			HttpResponse response = getClient().execute(get);
+			HttpEntity entity = response.getEntity();
+			
+			for (Header header : response.getAllHeaders()) {
+				if (header.getName().equals("Content-Encoding")) {
+					if (header.getValue().toLowerCase().equals("gzip")) {
+						entity = new GzipDecompressingEntity(entity);
+					}
+				}
+			}
+
+			content = BufferedInReader.read(
+					entity.getContent(),
+					StringUtil.isEmpty(charset) ? StringUtil.UTF8: charset
+			);
+		} catch (Exception e) {
+			if (tryAgain) {
+				Logger.printStackTrace("[" + url + "]内容采集出错：" + e.getMessage() + "，将重新尝试一次", e);
+				
+				return getUrlContent(url, charset, catchSleep, false);
+			} else {
+				Logger.printStackTrace("[" + url + "]内容采集出错：" + e.getMessage() + "", e);
+			}
+		}
+
+		
+		return content;
+	}
+	
+	
+	/**
+	 * 拼接 URL。
+	 * 
+	 * @param base
+	 * @param contact
+	 * @return
+	 */
+	public static String contactUrl(String base, String contact) {
+		
+		base = base.trim().replaceAll("\\\\", "");
+		contact = contact.trim().replaceAll("\\\\", "");
+
+		return UrlUtil.contact(base, contact);
+//		try {
+//			return new URL(new URL(base), contact).toString();
+//		} catch (Exception e) {
+//			return UrlUtil.contact(base, contact);
+//		}
+	}
+
+}
